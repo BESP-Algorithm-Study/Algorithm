f05800b8d58ebdca2ccb5b17b9c46af3a356a67d
W.llama <106076670+W-llama@users.noreply.github.com>
2025-06-01 22:53:01 +0900

    1주차: 배열 문제 풀이

---
+++ Array/10807-개수세기.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int a = Integer.parseInt(br.readLine());
+		int b = Integer.parseInt(br.readLine());
+		int c = Integer.parseInt(br.readLine());
+
+		int result = a * b * c;
+
+		int[] arr = new int[10];
+		String str = Integer.toString(result);
+
+		for (
+			int i = 0; i < str.length(); i++) {
+			char ch = str.charAt(i);
+			arr[ch - 48]++;
+		}
+
+		for (
+			int i = 0; i < 10; i++) {
+			System.out.println(arr[i]);
+		}
+	}
+}
